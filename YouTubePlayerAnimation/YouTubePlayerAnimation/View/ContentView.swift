d92cd7f6b1025876087161c01fdaed53b533bcb0
TsengPinRuei <tsengpinruei@gmail.com>
2024-12-16 20:01:06 +0800

    Add files via upload

---
+++ YouTubePlayerAnimation/YouTubePlayerAnimation/View/ContentView.swift
@@ -0,0 +1,14 @@
+//
+//  ContentView.swift
+//  YouTubePlayerAnimation
+//
+//  Created by 曾品瑞 on 2024/3/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
